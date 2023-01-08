ea96414c28c93c80d32969ad7401f342682189e5
jensen yuen <jensenyuen@outlook.com>
2023-01-09 06:21:53 +0900

    デサイン編集

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import Footer from '../components/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
   const text = {
     leftContentTitle: 'マルチステップフォーム',
-    leftContentSub: 'このサイトは前に担当した開発です',
-    rightContent: 'placeholder',
-    linkText: '使ってみよう'
+    leftContentSub: 'このサイトは前に担当した開発です。',
+    projName: 'Offgrid',
+    projDescription: 'Offgridは、登山の初心者向けに作成しました。',
+    projLink: 'https://github.com/eztree/offgrid',
+    linkText: '使ってみませんか'
   }
 
   return (
@@ -15,12 +19,23 @@ const Home = () => {
       <main className={styles.main}>
         <div className={styles.container}>
           <div className={`${styles.leftContent} ${styles.heroBanner}`}>
-            <h2>{text.leftContentTitle}</h2>
-            <p>{text.leftContentSub}</p>
+            <div className={`${styles.wrapper} ${styles.mb1}`}>
+              <h2 className={styles.mr1}>{text.projName}</h2>
+              <a href={text.projLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className='fs-4 mx-2'
+                style={{ marginRight: '1rem' }}
+              >
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </div>
+            <p className={styles.mb1}>{text.projDescription}</p>
           </div>
           <div className={styles.rightContent}>
-            {text.rightContent}
-            <button>
+            <h2 className={styles.mb1}>{text.leftContentTitle}</h2>
+            <p className={styles.mb1}>{text.leftContentSub}</p>
+            <button className={styles.button}>
               <Link href='/signup/datePeople'>{text.linkText}</Link>
             </button>
           </div>
